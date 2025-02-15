5cfd0c1a9664180e0fbe25eb0faa45223ee3719e
ibrahim yeryaran <ibrahimyeryaran93@gmail.com>
2025-02-16 01:55:22 +0300

    feat: add all task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text, key) {
+    if (!text || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const result = [];
+    const formattedText = text.toUpperCase();
+    const formattedKey = key.toUpperCase().repeat(Math.ceil(text.length / key.length)).slice(0, text.length);
+    let keyIndex = 0;
+
+    for (let i = 0; i < formattedText.length; i++) {
+      const currentChar = formattedText[i];
+      if (currentChar >= 'A' && currentChar <= 'Z') {
+        const shiftedChar = (currentChar.charCodeAt(0) - 65 + (formattedKey[keyIndex].charCodeAt(0) - 65)) % 26 + 65;
+        result.push(String.fromCharCode(shiftedChar));
+        keyIndex++;
+      } else {
+        result.push(currentChar);
+      }
+    }
+
+    if (!this.isDirect) {
+      result.reverse();
+    }
+
+    return result.join('');
+  }
+
+  decrypt(encryptedText, key) {
+    if (!encryptedText || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const result = [];
+    const formattedText = encryptedText.toUpperCase();
+    const formattedKey = key.toUpperCase().repeat(Math.ceil(encryptedText.length / key.length)).slice(0, encryptedText.length);
+    let keyIndex = 0;
+
+    for (let i = 0; i < formattedText.length; i++) {
+      const currentChar = formattedText[i];
+      if (currentChar >= 'A' && currentChar <= 'Z') {
+        const shiftedChar = (currentChar.charCodeAt(0) - formattedKey[keyIndex].charCodeAt(0) + 26) % 26 + 65;
+        result.push(String.fromCharCode(shiftedChar));
+        keyIndex++;
+      } else {
+        result.push(currentChar);
+      }
+    }
+
+    if (!this.isDirect) {
+      result.reverse();
+    }
+
+    return result.join('');
   }
 }
 
