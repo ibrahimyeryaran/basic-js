c2b715539cdc317b0d8add9c82e86c1e0cffef15
ibrahim yeryaran <ibrahimyeryaran93@gmail.com>
2025-02-16 02:05:42 +0300

    feat: add all task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,86 +1,75 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  constructor(isDirect = true) {
-    this.isDirect = isDirect;
+  constructor(direct = true) {
+    this.direct = direct;
   }
 
-  encrypt(text, key) {
-    if (!text || !key) {
-      throw new Error('Incorrect arguments!');
-    }
+  encrypt(message, key) {
+    if (!message || !key) throw new Error("Incorrect arguments!");
 
     const result = [];
-    const formattedText = text.toUpperCase();
-    const formattedKey = key.toUpperCase().repeat(Math.ceil(text.length / key.length)).slice(0, text.length);
     let keyIndex = 0;
+    message = message.toUpperCase();
+    key = key.toUpperCase();
 
-    for (let i = 0; i < formattedText.length; i++) {
-      const currentChar = formattedText[i];
-      if (currentChar >= 'A' && currentChar <= 'Z') {
-        const shiftedChar = (currentChar.charCodeAt(0) - 65 + (formattedKey[keyIndex].charCodeAt(0) - 65)) % 26 + 65;
-        result.push(String.fromCharCode(shiftedChar));
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(/[A-Z]/)) {
+        const shift = key[keyIndex % key.length].charCodeAt(0) - 65;
+        const charCode = ((message[i].charCodeAt(0) - 65 + shift) % 26) + 65;
+        result.push(String.fromCharCode(charCode));
         keyIndex++;
       } else {
-        result.push(currentChar);
+        result.push(message[i]);
       }
     }
 
-    if (!this.isDirect) {
-      result.reverse();
-    }
-
-    return result.join('');
+    return this.direct ? result.join("") : result.reverse().join("");
   }
 
-  decrypt(encryptedText, key) {
-    if (!encryptedText || !key) {
-      throw new Error('Incorrect arguments!');
-    }
+  decrypt(message, key) {
+    if (!message || !key) throw new Error("Incorrect arguments!");
 
     const result = [];
-    const formattedText = encryptedText.toUpperCase();
-    const formattedKey = key.toUpperCase().repeat(Math.ceil(encryptedText.length / key.length)).slice(0, encryptedText.length);
     let keyIndex = 0;
+    message = message.toUpperCase();
+    key = key.toUpperCase();
 
-    for (let i = 0; i < formattedText.length; i++) {
-      const currentChar = formattedText[i];
-      if (currentChar >= 'A' && currentChar <= 'Z') {
-        const shiftedChar = (currentChar.charCodeAt(0) - formattedKey[keyIndex].charCodeAt(0) + 26) % 26 + 65;
-        result.push(String.fromCharCode(shiftedChar));
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(/[A-Z]/)) {
+        const shift = key[keyIndex % key.length].charCodeAt(0) - 65;
+        const charCode =
+          ((message[i].charCodeAt(0) - 65 - shift + 26) % 26) + 65;
+        result.push(String.fromCharCode(charCode));
         keyIndex++;
       } else {
-        result.push(currentChar);
+        result.push(message[i]);
       }
     }
 
-    if (!this.isDirect) {
-      result.reverse();
-    }
-
-    return result.join('');
+    return this.direct ? result.join("") : result.reverse().join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
